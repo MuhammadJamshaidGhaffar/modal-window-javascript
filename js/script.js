ecf5c724a73cd8a99c153da346b3e9a1f186115b
Muhammad Jamshaid Ghaffar <muhammadjamshaid89@gmail.com>
2021-07-15 15:28:37 +0500

    Completed the project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function showModalWindow(){
+    let modalWindow = document.getElementById("modal-window");
+    modalWindow.style.display = "block";
+}
+
+function closeModalWindow(){
+    let modalWindow = document.getElementById("modal-window");
+    modalWindow.style.display = "none";
+}
+
